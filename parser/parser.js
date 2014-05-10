9213f0cf277cfc6592cf1b56491e3a40a61648e6
Tim Welch <tim.j.welch@gmail.com>
2014-05-09 23:03:58 -0700

    add simple parse

---
+++ parser/parser.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+fs.readFile('log.txt', function(err, logData) {
+	if (err) throw err;
+	var text = logData.toString();
+	var lines = text.split('\n');
+	var results = {};
+	lines.forEach(function(line) { 
+		var parts = line.split(' ');
+		var letter = parts[1];
+		var count = parts[2];
+
+		if (!results[letter]) {
+			results[letter] = 0;
+		}
+		results[letter] += parseInt(count);
+	});
+	console.log(results);
+});
